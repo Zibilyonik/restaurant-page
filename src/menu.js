0848091c3752e2e9fa2f4f528d902812a74075cf
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-06-14 01:46:17 +0200

    -Add placeholder text for tabs.

--- src/menu.js
+++ src/menu.js
@@ -2,9 +2,13 @@ function createMenu() {
   const main = document.querySelector('#Content');
   const container = document.createElement('div');
   container.classList.add('menu-container', 'container', 'border', 'rounded', 'text-center');
-  const temp = document.createElement('h2');
+  let temp = document.createElement('h2');
   temp.classList.add('h2', 'title');
   temp.innerHTML = 'Bonanza\'s Special Menu';
+  container.appendChild(temp);
+  temp = document.createElement('p');
+  temp.classList.add('h4', 'mx-3', 'my-5');
+  temp.innerHTML = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt quod expedita temporibus sequi dicta ad, numquam vero dolores earum voluptatem voluptates animi natus amet molestias modi totam provident illo commodi tempore atque! In eveniet, accusantium ut ea veritatis nihil illum labore temporibus! Ea quam modi praesentium, aliquid quas alias esse!';
   container.appendChild(temp);
   main.appendChild(container);
 }
